88127c71da6fa8a0c2082fca282c0a851ab9c3ae
Renuupendra Sulthan <renuupendrasulthan1@gmail.com>
2024-12-22 18:47:56 -0500

    supports the linear search for the input string array

--- arrays/src/LinearSearchString.java
+++ arrays/src/LinearSearchString.java
@@ -4,13 +4,44 @@ import java.util.Scanner;
 public class LinearSearchString {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter input string: ");
-        String inputString = sc.nextLine().toLowerCase();
-        System.out.println("Enter the Character to search in the input string");
-        char findChar = sc.next().toLowerCase().charAt(0);
 
-        searchChar(inputString, findChar);
+        System.out.println("Enter choice to search \n 1.character\n 2.String");
+        int value = sc.nextInt();
+        sc.nextLine();
+        switch (value){
+            case 1 -> {
+                System.out.println("Enter input string: ");
+                String inputString = sc.nextLine().toLowerCase();
+                System.out.println("Enter the Character to search in the input string");
+                char findChar = sc.next().toLowerCase().charAt(0);
+                searchChar(inputString, findChar);
+            }
 
+            case 2-> {
+                System.out.println("Enter array size: ");
+                int arraySize = sc.nextInt();
+                sc.nextLine();
+                String[] array = new String[arraySize];
+                System.out.println("Enter array elements: ");
+
+                for (int i = 0; i < arraySize; i++) {
+                    array[i] = sc.nextLine();
+                }
+                System.out.println("Enter the  string element to find in the array");
+                String findString = sc.nextLine();
+                linearSearchString(array, findString);
+            }
+        }
+    }
+
+    private static void linearSearchString(String[] array, String findString) {
+        for (int i = 0; i < array.length; i++) {
+            if (findString.equalsIgnoreCase(array[i])){
+                System.out.println("Given string found at the index: "+i);
+                return;
+            }
+        }
+        System.out.println("String not found");
     }
 
     private static void searchChar(String inputString, char findChar) {
